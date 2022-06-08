bb1a50f5b99c728d0409965a7e18cb38578d5e8d
pintti <allu.tuovinen@gmail.com>
2022-06-08 18:41:52 +0300

    lil fix

--- arm_controller.py
+++ arm_controller.py
@@ -1,5 +1,5 @@
 import subprocess
 
-COMMAND = ["ros2", "action", "send_goal", "/head_controller/follow_joint_trajectory control_msgs/action/FollowJointTrajectory", "{ trajectory: { joint_names: [head_pan_joint, head_tilt_right_joint, head_tilt_left_joint, head_tilt_vertical_joint], points: [ { positions: [0.5, 0.0, 0.0, 0.0], time_from_start: { sec: 1, nanosec: 0 } }]}}"]
+COMMAND = ["ros2", "action", "send_goal", "/head_controller/follow_joint_trajectory", "control_msgs/action/FollowJointTrajectory", "{ trajectory: { joint_names: [head_pan_joint, head_tilt_right_joint, head_tilt_left_joint, head_tilt_vertical_joint], points: [ { positions: [0.5, 0.0, 0.0, 0.0], time_from_start: { sec: 1, nanosec: 0 } }]}}"]
 
 subprocess.run(COMMAND)
